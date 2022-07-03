220ad1d60a27c0d1effdff15576cc67ba305dd97
Kevin Zheng <kevzheng27@gmail.com>
2022-07-02 20:01:54 -0400

    commit tree debb4eb52f9349a83f34a36706adb696ece7cb37

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 /* Your Code Here */
 
+function createEmployeeRecord(testEmployee) {
+    return {  
+    firstName: testEmployee[0],
+    familyName: testEmployee[1],
+    title: testEmployee[2],
+    payPerHour: testEmployee [3],
+    timeInEvents: [],
+    timeOutEvents: [],
+    } }
+
+function createEmployeeRecords(twoRows) {
+    return twoRows.map(row => createEmployeeRecord(row))
+    }
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    let Obj ={ 
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date,
+    }
+   let newEvent = this.timeInEvents
+   newEvent.push(Obj)
+    return this
+ }
+
+ function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    let Obj ={ 
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date,
+    }
+   let newEvent = this.timeOutEvents
+   newEvent.push(Obj)
+    return this
+ }
+
+ function hoursWorkedOnDate(dateStamp) {
+    const timeIn = this.timeInEvents.find(event => event.date === dateStamp)
+    const timeOut = this.timeOutEvents.find(event => event.date === dateStamp)
+
+    return (timeOut.hour - timeIn.hour)/100
+ }
+
+ function wagesEarnedOnDate(dateStamp) {
+    const hours = hoursWorkedOnDate.call(this, dateStamp)
+    return this.payPerHour * hours
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +71,10 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(employees, firstNameString) {
+    return employees.find(emp => emp.firstName === firstNameString)
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.map(employee => allWagesFor.call(employee)).reduce((currentValue, total) => currentValue + total)
+}
